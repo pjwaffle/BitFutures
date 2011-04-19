1db67501911c76e0f59a0b65831b133928d234f5
Tyson Key <tyson.key@gmail.com>
2011-04-19 21:13:28 +0100

    Fix a minor typo.

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <html>
 
 	<head>
-		<title>BitFuture Platform Installation</title>
+		<title>BitFutures Platform Installation</title>
 	</head>
 
 	<body>
 
 <?php
 
-	echo("Welcome to the BitFuture Platform!");
+	echo("Welcome to the BitFutures Platform!");
 	echo("This page exists to ensure that your deployment environment is functional.");
 
 ?>
