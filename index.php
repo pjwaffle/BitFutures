f015d166737b683f82c245dec8c880eefa220f76
Tyson Key <tyson.key@gmail.com>
2011-04-19 21:08:54 +0100

    An initial attempt at a welcome page.

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+	<head>
+		<title>BitFuture Platform Installation</title>
+	</head>
+
+	<body>
+
+<?php
+
+	echo("Welcome to the BitFuture Platform!");
+	echo("This page exists to ensure that your deployment environment is functional.");
+
+?>
+
+	</body>
+</html>
