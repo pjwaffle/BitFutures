47f1b485632041fff058e3ba18b43149c83fc20f
Tyson Key <tyson.key@gmail.com>
2011-04-22 19:34:28 +0100

    Add a .GitIgnore file for dealing with Eclipse-related stuff; and add the XHTML DocType declaration.

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html>
 
 	<head>
@@ -5,6 +8,8 @@
 	</head>
 
 	<body>
+	
+	<img src="/styling/interlude/images/logo.png" />
 
 <?php
 
